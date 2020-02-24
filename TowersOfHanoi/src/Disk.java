79a974c664d705e346f649c09eaba5319e09542a
Anuj <anujisawesome78@gmail.com>
2020-02-24 09:21:05 -0500

    First draft of Disk

--- TowersOfHanoi/src/Disk.java
+++ TowersOfHanoi/src/Disk.java
@@ -1,7 +1,6 @@
 //imports
 import java.awt.Color;
 import objectdraw.*;
-import objectdraw.ActiveObject;
 
 
 public class Disk extends ActiveObject{
@@ -13,7 +12,39 @@ public class Disk extends ActiveObject{
 	private Post post;
 	
 	//constructor
-	public Disk() {
+	
+	public Disk(double x, double y, double width, double height, DrawingCanvas c, int size, Color color, Post post) {
+		//making the rectangle
+		disk = new FilledRect(x, y, width, height, c);
 		
+		//declaring variables
+		this.size= size;
+		this.color = color;
+		this.post = post;
+	}
+	
+	public void toggleIsTop() {
+		
+		isTop = !isTop;
+		//setting isTop to opposite
+	}
+	
+	public boolean checkTop() {
+		
+		//boolean accessor method
+		return isTop;
+	}
+	
+	public void changePost(Post[] postArray, int postNumber) {
+		
+		//changing the post
+		post = postArray[postNumber];
+	}
+	
+	//Asking the WindowController where the mouse is
+	public void followCursor(double mouseX, double mouseY) {
+		
+		//moving the disk
+		disk.moveTo(mouseX, mouseY);
 	}
 }
