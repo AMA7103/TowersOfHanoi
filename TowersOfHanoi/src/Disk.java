1d348a9e9baa2de0b09dbb18126a452b4bf06bcb
Anuj <anujisawesome78@gmail.com>
2020-02-24 08:55:03 -0500

    Instance Variables

--- TowersOfHanoi/src/Disk.java
+++ TowersOfHanoi/src/Disk.java
@@ -1,4 +1,19 @@
+//imports
+import java.awt.Color;
+import objectdraw.*;
+import objectdraw.ActiveObject;
 
-public class Disk {
 
+public class Disk extends ActiveObject{
+	//instance variables
+	private int size;
+	private Color color;
+	private boolean isTop;
+	private FilledRect disk;
+	private Post post;
+	
+	//constructor
+	public Disk() {
+		
+	}
 }
